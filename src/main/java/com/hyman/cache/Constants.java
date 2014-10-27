c8db6c9261457b4177960113c4e42c6b448568dd
Hyman <hyman.xie@gmail.com>
2014-10-27 09:56:59 +0800

    add cache interfaces and redis implementation

---
+++ src/main/java/com/hyman/cache/Constants.java
@@ -0,0 +1,12 @@
+package com.hyman.cache;
+
+public final class Constants {
+	private Constants() {
+		
+	}
+	
+	public static final Integer REDIS_MGET_LIMIT=10;
+	
+	public static final Long REDIS_DEFAULT_CACHE_TIME=1209600000l;
+
+}
---
+++ src/main/java/com/hyman/cache/RootNamespace.java
@@ -0,0 +1,9 @@
+package com.hyman.cache;
+
+public interface RootNamespace {
+	
+	public static final String BAOCHOU_ROOT="cache";
+	
+	public static final String NAMESPACE_SEPARATOR=":";
+	
+}
